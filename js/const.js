0b5e2b759c0090e701d5ca819d008801a6f9cf38
sinkei0427 <91212239+sinkei0427@users.noreply.github.com>
2023-08-20 03:17:05 +0900

    Update const.js

--- js/const.js
+++ js/const.js
@@ -2,4 +2,4 @@
 // WSURL = 'wss://cataso2.omatz.cf';
 // WSURL = 'wss://cataso-omatz.herokuapp.com';
 // WSURL = 'ws://localhost:5000';
-WSURL = 'wss://omatzonline.onrender.com';
+WSURL = 'https://sinkeikunkataso.onrender.com';
